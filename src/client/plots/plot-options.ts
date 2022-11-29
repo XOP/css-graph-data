abdfb609d50c271ed77b5f0ba6195dcd82c7a577
XOP <wdybih@gmail.com>
2022-11-29 18:38:47 +0100

    global options

---
+++ src/client/plots/plot-options.ts
@@ -0,0 +1,16 @@
+import { COLORS } from '../../utils/globals';
+
+export const plotOptions = {
+  x: {
+    label: 'Date',
+  },
+  width: 900,
+  height: 500,
+  marginLeft: 45,
+  marginRight: 45,
+  grid: true,
+  style: {
+    background: 'transparent',
+    color: COLORS.light,
+  },
+};
---
+++ src/utils/globals.ts
@@ -0,0 +1,16 @@
+export const DIM_SIZE = 'size';
+export const DIM_SIZE_GZIP = 'sizeGzip';
+export const DIM_SELECTORS_AVG = 'selectorsAvg';
+export const DIM_SELECTORS_SUM = 'selectorsSum';
+export const DIM_COMMON_LINES = 'lines';
+export const DIM_COMMON_RULES = 'rules';
+export const DIM_DECL_AMOUNT = 'declarations';
+export const DIM_DECL_COHESION = 'cohesion';
+export const LAYOUT_GRID = 'grid';
+export const LAYOUT_FLOW = 'flow';
+
+export const COLORS = {
+  dark: '#0d0d0d',
+  light: '#c2ea34',
+  accent: '#f2f2f2'
+};
